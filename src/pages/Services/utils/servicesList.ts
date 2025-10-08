5c9d7b3e8d7c2d5e3191bea966b4d34d0c3bbdc7
Viktor Gordienko <vgordienko@team.vtbconnect.ru>
2025-10-08 18:25:09 +0300

    Added a new service package. Replaced the image

--- src/pages/Services/utils/servicesList.ts
+++ src/pages/Services/utils/servicesList.ts
@@ -1,5 +1,7 @@
 import { ReactComponent as Pc } from "../../../assets/svg/pc.svg";
 import { ReactComponent as Phone } from "../../../assets/svg/phone.svg";
+import { ReactComponent as ServicePackageThree } from "../../../assets/svg/servicePackageThree.svg";
+import { ReactComponent as ServicePackageSix } from "../../../assets/svg/servicePackageSix.svg";
 
 export const servicesList = [
   {
@@ -15,5 +17,19 @@ export const servicesList = [
     subtext: "Стоимость:\u00A02\u00A0000р.",
     number: "02",
     header: "Повторная\u00A0консультация",
+  },
+  {
+    img: ServicePackageThree,
+    text: "Встреча\u00A0длится 60\u00A0минут.",
+    subtext: "Стоимость:\u00A05\u00A0500р.",
+    number: "03",
+    header: "Пакет\u00A03\u00A0консультации",
+  },
+  {
+    img: ServicePackageSix,
+    text: "Встреча\u00A0длится 60\u00A0минут.",
+    subtext: "Стоимость:\u00A010\u00A0500р.",
+    number: "04",
+    header: "Пакет\u00A06\u00A0консультаций",
   },
 ];

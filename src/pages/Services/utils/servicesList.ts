c2800ecf5062cc33cc5841f3d4899f85c494a91a
Viktor Gordienko <vgordienko@team.vtbconnect.ru>
2026-08-28 16:35:47 +0300

    Change price list

--- src/pages/Services/utils/servicesList.ts
+++ src/pages/Services/utils/servicesList.ts
@@ -5,14 +5,14 @@ export const servicesList = [
   {
     img: Phone,
     text: "Встреча\u00A0длится 60\u00A0минут.",
-    subtext: "Стоимость:\u00A02\u00A0500р.",
+    subtext: "Стоимость:\u00A03\u00A0000р.",
     number: "01",
     header: "Консультация",
   },
   {
     img: Pc,
     text: "Встреча\u00A0длится 90\u00A0минут.",
-    subtext: "Стоимость:\u00A04\u00A0000р.",
+    subtext: "Стоимость:\u00A05\u00A0000р.",
     number: "02",
     header: "Семейная\u00A0терапия",
   },
